9a36fbd42bc0c46aef8b0b0d03cac0bbc0c26f03
Fábio Rhormens Vicente Júnior <fabiorhormensj@gmail.com>
2026-01-12 18:01:32 -0300

    creation of SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,8 @@ public class Main {
     public static void main(String[] args) {
         Department department = new Department(2, "Books");
         Seller seller = new Seller(1, "João", "joao@gmail.com", new Date(), 2000.0, department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
